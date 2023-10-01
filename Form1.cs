a36f7512e1f64744169bd47b3f183599c29113b3
Onur Kantar <Onur.Kantar@logo.com.tr>
2023-10-01 16:59:19 +0300

    dizayn seç kısmı eklendi

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+﻿using System.Windows.Forms;
+
 namespace TiMakWinApp
 {
     public partial class Form1 : Form
@@ -5,6 +7,27 @@ namespace TiMakWinApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "STL|*.stl";
+            file.RestoreDirectory = true;
+            file.CheckFileExists = false;
+            file.Title = "STL Dosyası Seçiniz..";
+            file.Multiselect = true;
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                string DosyaYolu = file.FileName;
+                string DosyaAdi = file.SafeFileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
